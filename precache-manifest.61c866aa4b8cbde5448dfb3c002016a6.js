7faabc52fd1b3d8940c11c96e531172e91ccf54a
sourabh chauhan <sourabh1202@gmail.com>
2020-09-03 19:49:43 +0530

    Updates

---
+++ precache-manifest.61c866aa4b8cbde5448dfb3c002016a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5677a155b21ce01e4de0c12b881a92a8",
+    "url": "/CollegeDunia/index.html"
+  },
+  {
+    "revision": "81b075b3fc06d469dfd5",
+    "url": "/CollegeDunia/static/css/main.5b9178cc.chunk.css"
+  },
+  {
+    "revision": "5e966562b289177e52bf",
+    "url": "/CollegeDunia/static/js/2.8aa970fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CollegeDunia/static/js/2.8aa970fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81b075b3fc06d469dfd5",
+    "url": "/CollegeDunia/static/js/main.e890cc04.chunk.js"
+  },
+  {
+    "revision": "13e4453b055f463b8555",
+    "url": "/CollegeDunia/static/js/runtime-main.6be0e371.js"
+  },
+  {
+    "revision": "619ee97eae1c5e93f5d68a9157b2466c",
+    "url": "/CollegeDunia/static/media/college_00.619ee97e.jpg"
+  },
+  {
+    "revision": "8793ec0ee96ac6622dd5064036daac2a",
+    "url": "/CollegeDunia/static/media/college_01.8793ec0e.jpg"
+  }
+]);
